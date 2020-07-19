e0e1dfa83d27fd63bcde4110bb7e3fc7d50269a3
ljubab <60656518+ljubab@users.noreply.github.com>
2020-07-19 20:59:29 +0200

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
+//find_by_order, order_of_key
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<vpii> vvpii;
+typedef long long ll;
+typedef long double ld;
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend();
+#define sz(x) (int)(x).size()
+
+const int INF32 = 1e9;
+const ll INF64 = 1e18;
+
+const int dx[4] = {1, -1, 0, 0};
+const int dy[4] = {0, 0, 1, -1};
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	
+
+	return 0;
+}
