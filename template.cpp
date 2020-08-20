32bb9c17eb841caaeed1e9b575011ed848d63a0e
ljubab <60656518+ljubab@users.noreply.github.com>
2020-08-20 20:55:48 +0200

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,6 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
-using namespace __gnu_pbds;
-
-typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> indexed_set;
-//find_by_order, order_of_key
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
@@ -17,25 +12,21 @@ typedef long double ld;
 
 #define pb push_back
 #define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
 #define fi first
 #define se second
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend();
-#define sz(x) (int)(x).size()
 
-const int INF32 = 1e9;
-const ll INF64 = 1e18;
-
-const int dx[4] = {1, -1, 0, 0};
-const int dy[4] = {0, 0, 1, -1};
+template<class T> bool ckmin(T &a, const T &b) {return a > b ? a = b, 1 : 0;}
+template<class T> bool ckmax(T &a, const T &b) {return a < b ? a = b, 1 : 0;}
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
-	
+    
 
-	return 0;
 }
