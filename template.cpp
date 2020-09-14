2c24f05a3a0ff1174cde55412fc0ce7ddf6a0e14
ljubab <60656518+ljubab@users.noreply.github.com>
2020-09-14 10:11:02 +0200

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
@@ -9,6 +12,8 @@ typedef vector<pii> vpii;
 typedef vector<vpii> vvpii;
 typedef long long ll;
 typedef long double ld;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 #define pb push_back
 #define mp make_pair
